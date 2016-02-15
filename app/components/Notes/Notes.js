3b33660b167236865ac524091c0003d896dfacf5
reindeers <velokarri@mail.ru>
2016-02-15 21:26:21 +0300

    test some es6

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,26 +1,23 @@
-var React = require('react');
-var NotesList = require('./NotesList');
-var AddNote = require('./AddNote');
+import React from 'react';
+import NotesList from './NotesList';
+import AddNote from './AddNote';
 
-var Notes = React.createClass({
+class Notes extends React.Component {
+  render(){
+    return (
+      <div>
+        <h3> Notes for {this.props.username} </h3>
+        <AddNote username={this.props.username} addNote={this.props.addNote} />
+        <NotesList notes={this.props.notes} />
+      </div>
+    )
+  }
+}
 
-	propTypes: {
-		username: React.PropTypes.string.isRequired,
-		notes: React.PropTypes.array.isRequired,
-		addNote: React.PropTypes.func.isRequired
+Notes.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  notes: React.PropTypes.array.isRequired,
+  addNote: React.PropTypes.func.isRequired,
+}
 
-	},
-	render: function(){
-
-		return (
-			<div>
-				<h3> Notes for {this.props.username} </h3>
-				<AddNote username = {this.props.username} addNote={this.props.addNote}/>
-				<NotesList notes={this.props.notes} />
-				
-			</div>
-		)
-	}
-});
-
-module.exports = Notes;
+export default Notes;
